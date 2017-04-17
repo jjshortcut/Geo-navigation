f53017dd472e4852229df69a4d6bab40e8ddcf40
JJS <jan-jaap_schuurman@hotmail.com>
2017-04-17 23:44:49 +0200

    Revert "Initial commit geonavigation device"
    This reverts commit 63b5158b9779185df5213a1b94b0dbea3473fbd8.

--- Main/Debug/vector.d
+++
@@ -1,6 +0,0 @@
-vector.d vector.o: .././vector.c .././vector.h \
- c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\math.h
-
-.././vector.h:
-
-c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\math.h:
